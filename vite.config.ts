252ca204748ac037405fed802c1459e7b3a1e0dd
Anway Ghatak <ghatakanway@gmail.com>
2026-01-12 12:44:19 +0530

    Prepare repo for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/humanity-memory-grid/',
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
